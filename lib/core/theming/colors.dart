08001023e9d054f288b99b9479aa5641cf652d05
Ahmed Ramadan <ahmed.ramadan20@outlook.com>
2025-09-20 17:45:54 +0300

    ok now connection works

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorsManager {
   static const Color mainBlue = Color(0xff247cff);
   static const Color lightBlue = Color(0xff2d9cdb);
+  static const Color darkBlue = Color(0xff1c5fb8);
 }
